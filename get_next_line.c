be85365582a569661066a0494411a96d3e8c71e2
Momox <119704058+momoox@users.noreply.github.com>
2023-02-28 15:32:52 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgeisler <mgeisler@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/25 18:01:31 by mgeisler          #+#    #+#             */
+/*   Updated: 2023/02/25 18:01:31 by mgeisler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
 char	*ft_buf(char *buffer)
@@ -25,11 +37,18 @@ char	*get_next_line(int fd)
 
 	onum = BUFFER_SIZE;
 	stock = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!stock)
+		return (0);
 	ft_strcpy(stock, buffer);
 	while (onum == BUFFER_SIZE)
 	{
 		onum = read(fd, buffer, BUFFER_SIZE);
 		buffer[onum] = '\0';
+		if (onum <= 0)
+		{
+			free(stock);
+			return (NULL);
+		}
 		if (ft_memchr(buffer, '\n', ft_strlen(buffer)))
 		{
 			stock = ft_strjoin(stock, buffer);
@@ -39,13 +58,13 @@ char	*get_next_line(int fd)
 		}
 		stock = ft_strjoin(stock, buffer);
 	}
-	stock[ft_strlen(stock) + 1] = '\0';
+	//stock[ft_strlen(stock)] = '\0';
 	return (stock);
 }
 
 // int	main()
 // {
-// 	int	fd = open("text2.txt", O_RDONLY);
+// 	int	fd = open("test.txt", O_RDONLY);
 
 // 	printf("%s\n", get_next_line(fd));
 // 	printf("%s\n", get_next_line(fd));
